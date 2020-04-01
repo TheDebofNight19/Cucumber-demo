3b4f706930838c80eb6537ecdf1161484f888044
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-04-01 11:21:04 +0300

    page obj version

---
+++ src/test/java/com/github/TheDebofNight19/LoginPage.java
@@ -0,0 +1,31 @@
+package com.github.TheDebofNight19;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class LoginPage {
+
+    private final static String URL = "https://idemo.bspb.ru";
+    private final static String LOGIN = "demo";
+    private final static String PASSWORD = "demo";
+
+    private SelenideElement loginField = $(By.name("username"));
+    private SelenideElement passwordField = $(By.name("password"));
+    private SelenideElement enterButton = $(By.id("login-button"));
+
+
+    public void login(){
+
+        Selenide.open(URL);
+        loginField.clear();
+        passwordField.clear();
+        loginField.setValue(LOGIN);
+        passwordField.setValue(PASSWORD);
+        enterButton.click();
+    }
+
+}
